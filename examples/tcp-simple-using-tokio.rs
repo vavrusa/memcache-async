012246fb0fa1990c632bfc4643e43426e5f44de0
v0s00jz <v0s00jz@homeoffice.wal-mart.com>
2022-07-23 01:38:44 -0700

    examples: adding tokio based example for tcp-simple

---
+++ examples/tcp-simple-using-tokio.rs
@@ -0,0 +1,27 @@
+use futures::io::AllowStdIo;
+use memcache_async::ascii::Protocol;
+use std::env;
+use std::net::TcpStream;
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("{} <addr>", args[0]);
+        return;
+    }
+
+    let addr = &args[1];
+
+    let (key, val) = ("foo", "bar");
+    let stream = TcpStream::connect(addr).expect("Failed to create stream");
+
+    let mut cache = Protocol::new(AllowStdIo::new(stream));
+    cache
+        .set(&key, val.as_bytes(), 0)
+        .await
+        .expect("Failed to set key");
+
+    let v = cache.get(&key).await.expect("Failed to get key");
+    assert_eq!(v, val.as_bytes());
+}
